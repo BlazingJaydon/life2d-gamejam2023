9d439dc601f29d36de66ad5c323f232fe0686047
Jaydon Hodge <jaydonhodge@gmail.com>
2023-05-24 17:12:49 -0400

    adjusted Slayers look mechanic

--- Assets/Scripts/Enemy/PatrolEnemyMovement.cs
+++ Assets/Scripts/Enemy/PatrolEnemyMovement.cs
@@ -13,6 +13,9 @@ public class PatrolEnemyMovement : MonoBehaviour
 
     public Animator animator;
 
+    bool isPatrolling;
+    bool isHunting;
+
     private void Awake()
     {
         animator.SetBool("isRunning", true);
@@ -28,23 +31,65 @@ public class PatrolEnemyMovement : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D collision) 
     {
-        if (collision.gameObject.CompareTag("Ground"))
+        if (collision.gameObject.CompareTag("Ground") && isPatrolling)
         {
             moveSpeed = -moveSpeed;
             FlipEnemyFacing();
         }
+        else if(collision.gameObject.CompareTag("Ground") && isHunting)
+        {
+            // Have the Slayer stand and wait for the player to come back to its level
+            //moveSpeed = 0;
+        }
+    }
+
+    private void statusCheck()
+    {
+        if (transform.GetComponent<EnemyVision>().detectPlayer(25f, 90f, 15, facingRight)) // If Slayer sees the Player
+        {
+            isHunting = true;
+            isPatrolling = false;
+        }
+        else
+        {
+            isHunting = false;
+            isPatrolling = true;
+        }
+    }
+
+    private void patrol()
+    {
+        enemyBody.velocity = new Vector2(moveSpeed, 0);
+    }
+
+    private void hunt()
+    {
+        float newSpeed = chasingSpeed;
+            if (moveSpeed < 0)
+                newSpeed *= -1;
+
+        enemyBody.velocity = new Vector2(newSpeed, 0);
+    }
+
+    private void Update() 
+    {
+        statusCheck();
     }
 
     void FixedUpdate()
     {
-        enemyBody.velocity = new Vector2(moveSpeed, 0);
-
-        if (transform.GetComponent<EnemyVision>().detectPlayer(5f, 90f, 15, facingRight))
+        if (isPatrolling)
         {
-            float newSpeed = chasingSpeed;
-            if (moveSpeed < 0)
-                newSpeed *= -1;
-            moveSpeed = newSpeed;
+            patrol();
         }
+        else if(isHunting)
+        {
+            hunt();
+        }
+    }
+
+    private void LateUpdate() 
+    {
+        
     }
 }
